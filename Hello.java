6b7724ba0091b04fd243d34048a6c221e8d455f9
bashar <bashars033@gmail.com>
2025-12-09 20:18:32 +0530

    Java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+// Sample Java program that prints "Hello, World!" to the console
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
