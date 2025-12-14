ec3d9f5af873eef359dcc1028bdeca7ad0bdb718
bashar <bashars033@gmail.com>
2025-12-14 20:58:29 +0530

    Arrays

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = {45, 2, 81, 99, 21, 11, 57};
+
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        System.out.println("Number of elements: " + numbers.length);
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
+}
