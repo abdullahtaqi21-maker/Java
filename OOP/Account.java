d51445857c23eb59cfdfc09cd1e25d573f3c0834
bashar <bashars033@gmail.com>
2026-01-07 20:18:03 +0530

    Java

--- OOP/Account.java
+++ OOP/Account.java
@@ -15,15 +15,15 @@ class SavingsAccount implements BankAccount {
     @Override
     public void deposit(double amount) {
         balance += amount;
-        System.out.println("Deposited: $" + amount);
+        System.out.println("Savings - Deposited: $" + amount);
     }
 
     @Override
     public void withdraw(double amount) {
         if (amount <= balance) {
-            System.out.println("Withdrawn: $" + amount);
+            System.out.println("Savings - Withdrawn: $" + amount);
         } else {
-            System.out.println("Insufficient balance");
+            System.out.println("Savings - Insufficient balance");
         }
     }
 
@@ -33,13 +33,49 @@ class SavingsAccount implements BankAccount {
     }
 }
 
+class CurrentAccount implements BankAccount {
+    private double balance;
+    private final double overdraftLimit = 100000;
+
+    public CurrentAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Current - Deposited: $" + amount);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (balance + overdraftLimit >= amount) {
+            balance -= amount;
+            System.out.println("Current - Withdrawn: $" + amount);
+        } else {
+            System.out.println("Current - Overdraft limit exceeded");
+        }
+    }
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+}
+
 public class Account {
     public static void main(String[] args) {
-        BankAccount account = new SavingsAccount(50000);
+        BankAccount savings = new SavingsAccount(5000);
+        BankAccount current = new CurrentAccount(50000);
 
-        account.deposit(20000);
-        account.withdraw(25000);
+        savings.deposit(1200);
+        savings.withdraw(4000);
 
-        System.out.println("Current Balance: $" + account.getBalance());
+        System.out.println("Savings - Balance: $" + savings.getBalance());
+        System.out.println();
+
+        current.deposit(1800);
+        current.withdraw(120000);
+
+        System.out.println("Current - Balance: $" + current.getBalance());
     }
 }
