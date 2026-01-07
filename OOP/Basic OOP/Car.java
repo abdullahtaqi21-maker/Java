1bd12ff66430c777b7765fcc697ae1176fa4013f
bashar <bashars033@gmail.com>
2026-01-07 20:57:17 +0530

    OOP

---
+++ OOP/Basic OOP/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    final int maxSpeed = 200;
+    int currentSpeed;
+
+    void setSpeed(int speed) {
+        if (speed > maxSpeed) {
+            currentSpeed = maxSpeed;
+        } else {
+            currentSpeed = speed;
+        }
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.setSpeed(250);
+        System.out.println("SPEED ===> " + myCar.currentSpeed);
+    }
+}
